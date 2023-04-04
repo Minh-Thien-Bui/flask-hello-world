c91c2058100f9188ccd19afdb32e56f0a9056392
Minh Bui <mibu4178@colorado.edu>
2023-04-04 08:19:36 +0000

    Connect Database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://bui_minh_db_user:L2TSBM9xSicOTaRmsIMVwBjPPh4ifjQC@dpg-cglto707oslael5ffs80-a/bui_minh_db")
+    conn.close()
+    return "Database Connection Successful"
