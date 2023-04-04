9130e193e425e57f881ba0b0a2367261c4a1ed0a
Minh Bui <mibu4178@colorado.edu>
2023-04-04 08:58:26 +0000

    Select Database

--- app.py
+++ app.py
@@ -44,3 +44,28 @@ def inserting():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgres://bui_minh_db_user:L2TSBM9xSicOTaRmsIMVwBjPPh4ifjQC@dpg-cglto707oslael5ffs80-a/bui_minh_db")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    
+    response_string = ""
+    response_string += "<table>"
+    
+    for player in records:
+        response_string += "<tr>"
+        
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+            
+        response_string += "<tr>"
+        
+    response_string += "<table>"
+    
+    return response_string
