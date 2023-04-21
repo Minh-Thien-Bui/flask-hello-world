1ad03aba8a60d24ce14b76479c19c2e2e8247f1b
Minh Bui <mibu4178@colorado.edu>
2023-04-21 22:28:17 +0000

    Test Create

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
+table_names = [
+    "account",
+    "body_part",
+    "equipment",
+    "exercise",
+    "favorite"
+]
+
+
 @app.route('/')
 def hello_world():
     return 'Hulk Smash!'
@@ -22,18 +31,10 @@ def testing():
     
 @app.route('/db_create')
 def creating():
-    conn = psycopg2.connect("postgres://hulk_user:sJ7uTRAXdhTsJQGOLD9Yq0uhsVBchdAE@dpg-cgrkvt1mbg5e4kh44l70-a.oregon-postgres.render.com/hulk")
+    conn = psycopg2.connect("postgres://bui_minh_db_user:L2TSBM9xSicOTaRmsIMVwBjPPh4ifjQC@dpg-cglto707oslael5ffs80-a/bui_minh_db")
     
     c = conn.cursor()
     directory = "models/"
-    
-    table_names = [
-        "account",
-        "body_part",
-        "equipment",
-        "exercise",
-        "favorite"
-    ]
     
     for table in table_names:
         path = directory + table + ".txt"
--- testing.py
+++ testing.py
@@ -1,24 +1,67 @@
 import sqlite3
 
-def print_table(cursor, table):
-    data = cursor.execute("SELECT * FROM " + table)
+table_names = [
+    "account",
+    "body_part",
+    "equipment",
+    "exercise",
+    "favorite"
+]
 
-    for row in data:
-        print(row)
+
+insertions = {
+    "body_part": [
+        (0, "Arms", "Bicep Curl"),
+        (1, "Back", "Rows"),
+        (2, "Legs", "Squats"),
+        (3, "Abs", "Sit-Ups"),
+        (4, "Cardio", "Running")
+    ],
+    
+    "equipment": [
+        (0, "None"),
+        (1, "Dumbells"),
+        (2, "Single Dumbell"),
+        (3, "Body Weight")
+    ],
+    
+    "exercise": [
+        (0, "Bicep Curl", "Curls dumbbells from a standing position", 
+         "Arms", "Dumbells"),
+        
+        (1, "Rows", "Pulls dumbbells towards the chest while bending over", 
+         "Back", "Dumbells"),
+        
+        (2, "Squats", "Lowers body by bending at the hips and knees", 
+         "Legs", "Body Weight"),
+        
+        (3, "Sit-Ups", 
+         "Lifts upper body towards knees while lying on the ground", 
+         "Abs", "Body Weight"),
+        
+        (4, "Running", "Fast-paced movement using legs and feet", 
+         "Cardio", "None")
+    ],
+    
+    "account": [
+        (0, "user1", "user1@example.com"),
+        (1, "user2", "user2@example.com"),
+        (2, "user3", "user3@example.com")
+    ],
+    
+    "favorite": [
+        (0, "user1", "Bicep Curl"),
+        (1, "user2", "Rows"),
+        (2, "user3", "Squats")
+    ]
+}
+
 
 def creating():
     conn = sqlite3.connect("test_tables")
     c = conn.cursor()
     
     directory = "models/"
-    
-    table_names = [
-        "account",
-        "body_part",
-        "equipment",
-        "exercise",
-        "favorite"
-    ]
     
     for table in table_names:
         path = directory + table + ".txt"
@@ -32,5 +75,51 @@ def creating():
         
     conn.commit()
     conn.close()
+
     
+def insert(value, table, c):
+    command = "INSERT INTO " + table + " VALUES ("
+    
+    col_count = c.execute(
+        "SELECT count() FROM PRAGMA_TABLE_INFO('" + table + "');"
+    )
+    
+    col_count = col_count.fetchall()
+    col_count = col_count[0][0]
+    
+    for i in range(col_count - 1):
+        command += "?, "
+    
+    command += "?);"
+    c.execute(command, value)
+    
+    
+def inserting():
+    conn = sqlite3.connect("test_tables")
+    c = conn.cursor()
+    
+    for table, value_list in insertions.items():
+        for value in value_list:
+            insert(value, table, c)
+            
+    conn.commit()
+    conn.close()
+    
+    
+def selecting():
+    conn = sqlite3.connect("test_tables")
+    c = conn.cursor()
+    
+    for table in table_names:
+        data = c.execute("SELECT * FROM " + table)
+
+        for row in data:
+            print(row)
+            
+        print()
+
+    conn.close()
+
 creating()
+inserting()
+selecting()
