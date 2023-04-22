82c839185611bef852d9a9e53675f9fc82b6c839
Minh Bui <mibu4178@colorado.edu>
2023-04-22 00:17:28 +0000

    Get Tables

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ def testing():
     conn = psycopg2.connect("postgres://hulk_user:sJ7uTRAXdhTsJQGOLD9Yq0uhsVBchdAE@dpg-cgrkvt1mbg5e4kh44l70-a.oregon-postgres.render.com/hulk")
     
     cur = conn.cursor()
-    cur.execute("SHOW TABLES")
+    cur.execute("""SELECT table_name FROM information_schema.tables
+       WHERE table_schema = 'public'""")
+    
     all_tables = ""
 
-    for table in cur:
+    for table in cur.fetchall():
         all_tables += table + "\n"
     
     conn.close()
